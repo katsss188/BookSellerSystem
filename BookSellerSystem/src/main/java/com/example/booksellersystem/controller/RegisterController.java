548de707d8cb84c4181762ee4b9d033f57f05b5f
bban <2948242244@qq.com>
2023-06-27 21:51:24 +0800

    完整版

---
+++ BookSellerSystem/src/main/java/com/example/booksellersystem/controller/RegisterController.java
@@ -0,0 +1,23 @@
+package com.example.booksellersystem.controller;
+
+import com.example.booksellersystem.pojo.Account;
+import com.example.booksellersystem.pojo.Result;
+import com.example.booksellersystem.service.RegisterService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//如名，注册部分
+@RestController
+public class RegisterController {
+
+    @Autowired
+    RegisterService registerService;
+    @RequestMapping("/register")
+    public Result register(@RequestBody Account account){
+       if(registerService.register(account) == 1)
+           return Result.success();
+        return Result.error("该用户已经注册");
+    }
+}
